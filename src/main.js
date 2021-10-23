3b870c1e632cca2ed4cf9621584321b0917e0f13
TienDN <duytienabc123@gmail.com>
2021-10-23 16:38:27 +0700

    Success UI

--- src/main.js
+++ src/main.js
@@ -14,3 +14,42 @@ function carousel() {
   x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); 
 }
+
+const exampleModal = document.getElementById("exampleModal");
+exampleModal.addEventListener("show.bs.modal", function (event) {
+  const button = event.relatedTarget;
+  const recipient = button.getAttribute("data-bs-whatever");
+  const tab_login = document.querySelector('.tab-login');
+  const tab_register = document.querySelector(".tab-register");
+  const active = document.querySelector(".active");
+  if(recipient === 'login') {
+    tab_register.style.display = 'none';
+    tab_login.style.display = "unset";
+  }
+  if(recipient === 'register') {
+    tab_register.style.display = "unset";
+    tab_login.style.display = "none";
+  }
+  
+  // const login = document.getElementById('login');
+  // const register = document.getElementById("register");
+  // console.log(active);
+  // login.addEventListener("click", (event) => {
+  //   console.log(event);
+  //   if (active) {
+  //     active.classList.remove("active");
+  //   }
+  //   else {
+  //     event.target.className = "active";
+  //   }
+
+  // });
+  // register.addEventListener("click", (event) => {
+  //   if (active) {
+  //     active.classList.remove("active");
+  //   } else {
+  //     event.target.classList.add("active");
+  //   }
+  // });
+});
+
